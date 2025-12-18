9a274fb05d6cd7070cabe84c762ec15ae38d9415
Raghvendra Singh <raghavsingh0027@gmail.com>
2025-12-18 09:10:01 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+
+import type {NextConfig} from 'next';
+import path from 'path';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  serverExternalPackages: ['@genkit-ai/*'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+export default nextConfig;
