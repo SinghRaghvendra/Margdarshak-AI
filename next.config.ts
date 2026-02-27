fe95e004b4226b54261376e9e2433dfb0f10665f
Raghvendra Singh <raghavsingh0027@gmail.com>
2026-02-27 18:27:26 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

--- next.config.ts
+++ next.config.ts
@@ -16,18 +16,22 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
       },
     ],
   },
